33d11d8f6652970fdc3cc2da636fa111b0c5cc77
Johann Schwabe <johann.schwabe@uzh.ch>
2023-06-12 12:47:39 +0200

    fixed unordered again

--- tpch.py
+++ tpch.py
@@ -7,7 +7,7 @@ from cascade import run
 from Query import Query
 from Relation import Relation
 
-dataset_version = ["unordered1", "unordered10"]
+dataset_version = ["_unordered1", "_unordered10"]
 base_dataset = "tpch"
 
 Part = Relation("part", OrderedSet(
